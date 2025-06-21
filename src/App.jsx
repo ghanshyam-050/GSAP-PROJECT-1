bc5165a9f5e9de06e116c0e2123ce72100454e47
ghanshyam-050 <ghanshyam991393gmail.com>
2025-06-21 19:09:38 +0530

    all image download

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">App</div>
+   <main>
+    <Navbar />
+   </main>
   )
 }
 
